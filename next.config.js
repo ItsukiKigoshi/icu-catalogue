5cbd134803131d4a2659bc73e616d3e2e6cac171
ItsukiKigoshi <itsukikigoshi@gmail.com>
2023-11-22 10:44:37 +0900

    Update next.config.js output to "export"

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-};
-
-module.exports = nextConfig;
-
-/* @type {import('next').NextConfig} */
 
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+  output: "export",
 });
 
 module.exports = withPWA({
