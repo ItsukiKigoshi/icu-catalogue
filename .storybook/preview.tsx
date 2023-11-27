9a20c60094c2d2dbfb5ea51e5a5c3a4d67c03f53
ItsukiKigoshi <itsukikigoshi@gmail.com>
2023-11-27 23:53:40 +0900

    Add Storybook configuration

---
+++ .storybook/preview.tsx
@@ -0,0 +1,30 @@
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import "@mantine/core/styles.css";
+
+import { MantineProvider, useMantineColorScheme } from "@mantine/core";
+import { addons } from "@storybook/preview-api";
+import React, { useEffect } from "react";
+import { DARK_MODE_EVENT_NAME } from "storybook-dark-mode";
+
+const channel = addons.getChannel();
+
+function ColorSchemeWrapper({ children }: { children: React.ReactNode }) {
+  const { setColorScheme } = useMantineColorScheme();
+  const handleColorScheme = (value: boolean) =>
+    setColorScheme(value ? "dark" : "light");
+
+  useEffect(() => {
+    channel.on(DARK_MODE_EVENT_NAME, handleColorScheme);
+    return () => channel.off(DARK_MODE_EVENT_NAME, handleColorScheme);
+  }, [channel]);
+
+  return <>{children}</>;
+}
+
+export const decorators = [
+  (renderStory: any) => (
+    <ColorSchemeWrapper>{renderStory()}</ColorSchemeWrapper>
+  ),
+  (renderStory: any) => <MantineProvider>{renderStory()}</MantineProvider>,
+];
