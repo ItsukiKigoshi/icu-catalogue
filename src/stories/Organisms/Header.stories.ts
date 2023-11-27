b21df7e747bf8876d8b69427a2dd7f7bf5cf0f66
ItsukiKigoshi <itsukikigoshi@gmail.com>
2023-11-28 03:23:11 +0900

    Remove .eslintrc.json and update Header and RequirementTable stories

--- src/stories/Organisms/Header.stories.ts
+++ src/stories/Organisms/Header.stories.ts
@@ -15,4 +15,9 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    opened: false,
+    toggle: () => {},
+  },
+};
