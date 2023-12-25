977841159be9c1d001b92ed786bf75e88452855d
ItsukiKigoshi <itsukikigoshi@gmail.com>
2023-12-25 22:43:17 +0900

    Update next.config.js with next-pwa configuration

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
-// /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
 
-// const withPWA = require("next-pwa")({
-//   dest: "public",
-//   register: true,
-//   skipWaiting: true,
-// });
-
-// module.exports = withPWA({
-//   nextConfig: {output: "export"},
-//   reactStrictMode: true,
-// });
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withPWA({
+  reactStrictMode: true,
+});
 
 module.exports = nextConfig;
