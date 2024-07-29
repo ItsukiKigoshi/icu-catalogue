ccf4b6fca67ce3fc7b79b352fd695b0e25e308eb
ItsukiKigoshi <itsukikigoshi@gmail.com>
2024-07-29 13:03:12 +0900

    chore: Update default selectedTermValue in Page component to "2024Autumn"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Page() {
       ],
     },
   ];
-  const [selectedTermValue, setSelectedTermValue] = useState("2024Spring");
+  const [selectedTermValue, setSelectedTermValue] = useState("2024Autumn");
   const selectedTerm: Term | undefined = terms
     .map((term) => term.items)
     .flat()
