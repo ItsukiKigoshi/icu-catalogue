36feeafa32e026202386d3dee084ea0f19169a98
ItsukiKigoshi <itsukikigoshi@gmail.com>
2025-02-21 16:11:54 +0900

    feat: add i18n

--- next.config.js
+++ next.config.js
@@ -4,9 +4,11 @@ const withPWA = require("next-pwa")({
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
 });
+const { i18n } = require('./next-i18next.config')
 
 const nextConfig = withPWA({
   reactStrictMode: true,
+  i18n
 });
 
 module.exports = nextConfig;
