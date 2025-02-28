3536ca0ebb9c22464e2a74bd8612ca6017279d60
ItsukiKigoshi <itsukikigoshi@gmail.com>
2025-03-01 00:37:34 +0900

    feat: add CourseSearch.tsx

---
+++ src/components/organisms/CourseSearch.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CourseSearch() {
+  // Dear Yifei, welcome to react! Itsuki
+  // Here will be JavaScript (Typescript)
+  return (
+    // Here will be extended version of HTML.
+    <div>CourseSearch</div>
+  );
+}
